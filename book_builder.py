4b8d9e857ac4929bb987ccff61c286e3548570b4
JouderMin <joudermin@foxmail.com>
2022-07-15 13:12:12 +0800

    修改了编译脚本，调整了位置

--- book_builder.py
+++ book_builder.py
@@ -1,19 +1,58 @@
 import os
+import sys
 
 
-def build_project(dir_path: str, project_name: str) -> None:
+def build_project_quick(dir_path: str, project_name: str) -> None:
     os.chdir(dir_path)
     os.system(
-        "xelatex --no-pdf -synctex=1 -interaction=nonstopmode -file-line-error -shell-escape " + project_name + ".tex")
+        "xelatex --no-pdf -synctex=1 -interaction=nonstopmode -file-line-error -shell-escape " +
+        project_name + ".tex"
+    )
     os.system(
-        "xelatex --no-pdf -synctex=1 -interaction=nonstopmode -file-line-error -shell-escape " + project_name + ".tex")
+        "xelatex --no-pdf -synctex=1 -interaction=nonstopmode -file-line-error -shell-escape " +
+        project_name + ".tex"
+    )
     os.system(
-        "xdvipdfmx -E " + project_name + ".xdv")
+        "xdvipdfmx -E -z0 -V 1.7 " + project_name + ".xdv")
     if os.name == "nt":
-        os.system("del *.bcf *.out *.xml *.toc *.xdv")
+        os.system("del *.bcf *.out *.xml *.toc *.xdv *.bbl *.blg")
     else:
-        os.system("rm *.bcf *.out *.xml *.toc *.xdv")
+        os.system("del *.bcf *.out *.xml *.toc *.xdv *.bbl *.blg")
+
+
+def build_project_bib(dir_path: str, project_name: str) -> None:
+    os.chdir(dir_path)
+    os.system(
+        "xelatex --no-pdf -synctex=1 -interaction=nonstopmode -file-line-error -shell-escape " +
+        project_name + ".tex"
+    )
+    os.system(
+        "bibtex " + project_name
+    )
+    os.system(
+        "xelatex --no-pdf -synctex=1 -interaction=nonstopmode -file-line-error -shell-escape " +
+        project_name + ".tex"
+    )
+    os.system(
+        "xelatex --no-pdf -synctex=1 -interaction=nonstopmode -file-line-error -shell-escape " +
+        project_name + ".tex"
+    )
+    os.system(
+        "xdvipdfmx -E -V 1.7 " + project_name + ".xdv"
+    )
+    if os.name == "nt":
+        os.system("del *.bcf *.out *.xml *.toc *.xdv *.bbl *.blg")
+    else:
+        os.system("rm *.bcf *.out *.xml *.toc *.xdv *.bbl *.blg")
 
 
 if __name__ == "__main__":
-    build_project("./书籍/基础篇/", "基础篇")
+    bib = False
+    for arg in sys.argv:
+        if arg == "-b":
+            bib = True
+
+    if bib:
+        build_project_bib("./书籍/基础篇/", "基础篇")
+    else:
+        build_project_quick("./书籍/基础篇/", "基础篇")
