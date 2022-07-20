370f46b759aaa89083399fc43acd422a3e5c6e9e
JouderMin <joudermin@foxmail.com>
2022-07-20 21:10:44 +0800

    重构书籍目录结构，增加程序设计篇

--- book_builder.py
+++ book_builder.py
@@ -1,49 +1,36 @@
 import os
 import sys
 
+xelatex_build_command = "xelatex --no-pdf -synctex=1 -interaction=nonstopmode -file-line-error -shell-escape "
+script_path=sys.path[0];
 
+# 快速编译，pdf 不进行压缩，不进行参考文献的加入
 def build_project_quick(dir_path: str, project_name: str) -> None:
     os.chdir(dir_path)
-    os.system(
-        "xelatex --no-pdf -synctex=1 -interaction=nonstopmode -file-line-error -shell-escape " +
-        project_name + ".tex"
-    )
-    os.system(
-        "xelatex --no-pdf -synctex=1 -interaction=nonstopmode -file-line-error -shell-escape " +
-        project_name + ".tex"
-    )
+    os.system(xelatex_build_command + project_name + ".tex")
+    os.system(xelatex_build_command + project_name + ".tex")
     os.system(
         "xdvipdfmx -E -z0 -V 1.7 " + project_name + ".xdv")
     if os.name == "nt":
         os.system("del *.bcf *.out *.xml *.toc *.xdv *.bbl *.blg")
     else:
         os.system("del *.bcf *.out *.xml *.toc *.xdv *.bbl *.blg")
+    os.chdir(script_path)
 
 
+# 完全编译
 def build_project_bib(dir_path: str, project_name: str) -> None:
     os.chdir(dir_path)
-    os.system(
-        "xelatex --no-pdf -synctex=1 -interaction=nonstopmode -file-line-error -shell-escape " +
-        project_name + ".tex"
-    )
-    os.system(
-        "bibtex " + project_name
-    )
-    os.system(
-        "xelatex --no-pdf -synctex=1 -interaction=nonstopmode -file-line-error -shell-escape " +
-        project_name + ".tex"
-    )
-    os.system(
-        "xelatex --no-pdf -synctex=1 -interaction=nonstopmode -file-line-error -shell-escape " +
-        project_name + ".tex"
-    )
-    os.system(
-        "xdvipdfmx -E -V 1.7 " + project_name + ".xdv"
-    )
+    os.system(xelatex_build_command + project_name + ".tex")
+    os.system("bibtex " + project_name)
+    os.system(xelatex_build_command + project_name + ".tex")
+    os.system(xelatex_build_command + project_name + ".tex")
+    os.system("xdvipdfmx -E -V 1.7 " + project_name + ".xdv")
     if os.name == "nt":
         os.system("del *.bcf *.out *.xml *.toc *.xdv *.bbl *.blg")
     else:
         os.system("rm *.bcf *.out *.xml *.toc *.xdv *.bbl *.blg")
+    os.chdir(script_path)
 
 
 if __name__ == "__main__":
@@ -54,5 +41,7 @@ if __name__ == "__main__":
 
     if bib:
         build_project_bib("./书籍/基础篇/", "基础篇")
+        build_project_bib("./书籍/程序设计篇/", "程序设计篇")
     else:
         build_project_quick("./书籍/基础篇/", "基础篇")
+        build_project_quick("./书籍/程序设计篇/", "程序设计篇")
